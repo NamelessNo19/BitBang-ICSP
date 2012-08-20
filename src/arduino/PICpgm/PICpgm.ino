a142c000ddce6dba4cb6a7d69b00563020cab515
Leon Hielscher <namelessno19@googlemail.com>
2012-08-20 22:47:08 +0200

    Implemented PIC erase functionality.

--- src/arduino/PICpgm/PICpgm.ino
+++ src/arduino/PICpgm/PICpgm.ino
@@ -201,20 +201,74 @@ inline uint16_t readWord(uint32_t memAdr)
   return res;
 }
 
-inline void enableFlashWrite()
+inline void setAccessToFlash()
 {
   cmdOut(CMD_OUT_CI, 0x8EA6);  // BSF  EECON1, EEPGD
-  cmdOut(CMD_OUT_CI, 0x8EA6);  // BCF  EECON1, CFGS
+  cmdOut(CMD_OUT_CI, 0x9CA6);  // BCF  EECON1, CFGS
 }
+
+inline void enableWrite()
+{
+	cmdOut(CMD_OUT_CI, 0x84A6);  // BSF  EECON1, WREN
+}
+
+inline void disableWrite()
+{
+	cmdOut(CMD_OUT_CI, 0x94A6);  // BCF  EECON1, WREN
+}
+
+inline void performRowErase()
+{
+	cmdOut(CMD_OUT_CI, 0x88A6);  // BSF  EECON1, FREE
+	cmdOut(CMD_OUT_CI, 0x82A6);  // BSF  EECON1, WR
+	clkFlashWrite();
+}
+
 
 void writeTest()
 {
-  enableFlashWrite();
+  setAccessToFlash();
   setTablePtr(0x000800);
   cmdOut(CMD_OUT_TBWR_POSI2, 0xFB0B);
   cmdOut(CMD_OUT_TBWR_SP_POSI2, 0xFACA);
   clkFlashWrite();
 }
+
+void bulkErase(const uint16_t erOpt)
+{
+	const uint8_t optLo = erOpt & 0x00FF;
+	const uint8_t optHi = (erOpt & 0xFF00) >> 8;
+
+	setTablePtr(MEM_BLKER_OPTH);
+	cmdOut(CMD_OUT_TBWR, (optHi << 8) & optHi);
+	setTablePtr(MEM_BLKER_OPTL);
+	cmdOut(CMD_OUT_TBWR, (optLo << 8) & optLo);
+
+	// Clocking erase
+
+	datOUT;
+	digitalWrite(pinPGD, LOW);
+	int i;
+	// 4 Clocks
+	for (i = 0; i < 4; i++)
+	  {
+	    clkHI; del; clkLO; del;
+
+	  }
+
+
+	delayMicroseconds(DELAY_P11);
+	delayMicroseconds(DELAY_P10);
+
+	// 16 Clocks
+	for (i = 0; i < 16; i++)
+	  {
+	    clkHI; del; clkLO; del;
+
+	  }
+}
+
+
   
   
   
