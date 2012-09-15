e7abd2504703ec6e31b08819e382200dd05f9b12
Leon Hielscher <namelessno19@googlemail.com>
2012-09-15 07:14:20 +0200

    Implemented 'echo' for PIC to improve code.

--- src/linux/pic18f/pic18fcon.c
+++ src/linux/pic18f/pic18fcon.c
@@ -7,12 +7,12 @@
 const char* CONF_REG_STR[] = {"CONFIG1L", "CONFIG1H", "CONFIG2L", "CONFIG2H", "CONFIG3H", "CONFIG4L",
 			  "CONFIG5L", "CONFIG5H", "CONFIG6L", "CONFIG6H", "CONFIG7L", "CONFIG7H"};
 
-int picIdent()
+int echo(unsigned char c1, unsigned char c2)
 {
-	printf("Sending identification request.\n");
-	serWrite("ID", 2);
-	unsigned char buf[13];
-	buf[0] = '\0'; buf[1] = '\0'; buf[2] = '\0';
+	unsigned char buf[3];
+	buf[0] = c1; buf[1] = c2; buf[2] = '\0';
+	serWrite(&buf, 2);
+	buf[0] = '\0'; buf[1] = '\0';
 	
 	if (!serRead(&buf[0], 3, TRUE)) 
 		return FALSE; 
@@ -20,12 +20,24 @@ int picIdent()
 	{
 		printf("Synchronization failed.\n");
 		return FALSE;
-	} else if (buf[0] != 'I' || buf[1] != 'D')
+	} else if (buf[0] != c1 || buf[1] != c2)
 	{
 		printf("Invalid response '%c%c'.\n", buf[0], buf[1]);
 		return FALSE;
 	}
 
+	return TRUE;
+}
+
+int picIdent()
+{
+	printf("Sending identification request.\n");
+
+
+	if (!echo('I', 'D'))
+		return FALSE;
+
+	unsigned char buf[13];
 	if (!serRead(buf, 3, TRUE))
 		return FALSE;
 
@@ -63,28 +75,10 @@ int picDumpBlock(unsigned char* blkData, unsigned char blockNo)
 {
 	unsigned char buf[33];
 	printf("Sending block %d read request.\n", blockNo);
-	serWrite("RB", 2);
-	serWrite(&blockNo, 1);
 	
-	if (!serRead(buf, 3, TRUE))
-		return FALSE;
-	else if (buf[0] == 'S' && buf[1] == 'F')
-	{
-		printf("Synchronization failed.\n");
-		return FALSE;
-	}
-	else if (buf[0] == 'I' && buf[1] == 'A')
-	{
-		printf("Invalid Block.\n");
-		return FALSE;
-	}	else if (buf[0] != 'R' || buf[1] != 'B')
-	{
-		printf("Invalid response '%c%c'.\n", buf[0], buf[1]);
-		return FALSE;
-	}
+	if (!echo('R', 'B'))
+			return FALSE;
 	
-	
-
 
 	printf("Receiving block data...");
 
@@ -113,101 +107,3 @@ int picDumpBlock(unsigned char* blkData, unsigned char blockNo)
 
 
 
-int tinyWritePage(unsigned char pageNo, unsigned char* data)
-{
-	unsigned char buf[5];
-	pageNo &= 0x1F;
-	serWrite("WP", 2);
-	serWrite(&pageNo, 1);
-	serWrite(data, 32);
-	printf("Transmitting data for page %d...\n", pageNo);
-	
-	
-	uint16_t crcCal = 0xFFFF;
-	int i;
-	for (i = 0; i < 32; i++) 
-	  crcCal = crc16_update(crcCal, data[i]);
-
-	
-	
-	
-	if (!serRead(buf, 5, TRUE))
-		return FALSE;
-	
-	if (buf[0] != 'W' || buf[3] != 'P')
-	{
-		printf("Invalid response '%c%c'.\n", buf[0], buf[3]);
-		return FALSE;
-	}
-	
-	uint16_t crcSer = 0x00 | (buf[1] << 8) | buf[2]; 
-	
-	if (crcSer == crcCal)
-		printf("Checksum OK. Start writing...\n");
-	else
-	{
-		printf("Checksum mismatch. Expected %#06x, got %#06x.\n", crcCal, crcSer);
-		printf("Cancel writing.\n");
-		serWrite("NA", 2);
-		return FALSE;
-	}
-	
-	serWrite("AC", 2);
-	
-	if (!serRead(buf, 3, TRUE))
-	{
-		printf("Verification timed out.\n");
-		return FALSE;
-	}	
-	else if (buf[0] == 'S' && buf[1] == 'F')
-	{
-		printf("Synchronization failed.\n");
-		return FALSE;
-	}
-	else if (buf[0] == 'I' && buf[1] == 'M')
-	{
-		printf("Device ID mismatch.\n");
-		return FALSE;
-	}
-	else if (buf[0] == 'C' || buf[1] == 'F')
-	{
-		printf("Verification failed. (Page already written?)\n");
-		return FALSE;
-	}
-	else if (buf[0] != 'A' || buf[1] != 'C')
-	{
-		printf("Unexpected response '%c%c'.\n", buf[0], buf[1]);
-		return FALSE;
-	}
-	
-	
-	
-	printf("Page verified.\n");
-	return TRUE;
-}
-
-int tinyChipErase()
-{
-	printf("Sending chip erase request.\n");
-	serWrite("ER", 2);
-
-	char buf[3];
-	if (!serRead(buf, 3, TRUE))
-			return FALSE;
-
-	if (buf[0] == 'E' && buf[1] == 'R')
-	{
-		printf("Chip erase confirmed.\n");
-		return TRUE;
-	}
-	else if (buf[0] == 'S' && buf[1] == 'F')
-	{
-		printf("Synchronization failed.\n");
-		return FALSE;
-	}
-	else
-	{
-		printf("Unexpected response '%c%c'.\n", buf[0], buf[1]);
-		return FALSE;
-	}
-}
